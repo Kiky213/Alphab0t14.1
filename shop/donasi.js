9071b4e1c538d167203d93dd7b18583ab4f57247
Kiky213 <98963949+Kiky213@users.noreply.github.com>
2022-02-06 22:08:39 +0700

    Update donasi.js

--- shop/donasi.js
+++ shop/donasi.js
@@ -1,25 +1,11 @@
 const donasibot = () => {
-return `DONASI BISA VIA BANK / E-MONEY
+return `DONASI PAKAI PULSA AJA KAKS
 
-*🏧 BANK*
-
-1. BRI 
-	ㅁ 8881 0887 4350 47326
-2. BCA 
-	ㅁ 3901 0887 4350 47326
-3. BNI 
-	ㅁ 881 0887 4350 47326
-4. PERMATA
-	ㅁ 852 8887 4350 47326
 	
 *💰 E-MONEY*
 
-1. GOPAY
-	ㅁ 0887 4350 47326
-2. OVO
-	ㅁ 0887 4350 47326
-3. DANA
-	ㅁ 0887 4350 47326
+1. Pulsa
+	ㅁ 6285210465076
 	
 Semuanya atas nama RI****DI
 `
